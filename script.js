7f32f91f7eaf9c5d1aacc70001fe892fd462a245
Sara Pedrosa <saraspedrosa.sp@gmail.com>
2021-05-24 09:15:29 -0300

    definindo movimentos

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right"; // direção da cobrinha
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -19,6 +20,29 @@ function criarCobrinha(){
     }
 
 }
-// chamando a função
-criarBG();
-criarCobrinha();
+
+function iniciarJogo() {
+    // chamando as funções
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction =="right") snakeX += box; // acrescenta um quadrado se a direção for pra direita 
+    if(direction ="left") snakeX -= box; // diminui um quadrado se a direção for esquerda
+    if(direction =="up") snakeY -= box;
+    if(direction =="down") snakeY += box;
+    
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarJogo, 100); // intervalo de tempo de criação do corpinho da cobrinha
+
