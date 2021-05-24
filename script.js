7f855bdba76eebcae308c2727a625eabcd963420
Sara Pedrosa <saraspedrosa.sp@gmail.com>
2021-05-24 09:42:25 -0300

    impedir que a cobrinha acabe ao tocar nos cantos

--- script.js
+++ script.js
@@ -21,18 +21,33 @@ function criarCobrinha(){
 
 }
 
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
     // chamando as funções
     criarBG();
     criarCobrinha();
+    
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
     if(direction =="right") snakeX += box; // acrescenta um quadrado se a direção for pra direita 
     if(direction ="left") snakeX -= box; // diminui um quadrado se a direção for esquerda
-    if(direction =="up") snakeY -= box;
-    if(direction =="down") snakeY += box;
+    if(direction =="up") snakeY -= box; // acima
+    if(direction =="down") snakeY += box; // abaixo
     
     snake.pop();
 
@@ -45,4 +60,5 @@ function iniciarJogo() {
 }
 
 let jogo = setInterval(iniciarJogo, 100); // intervalo de tempo de criação do corpinho da cobrinha
+
 
